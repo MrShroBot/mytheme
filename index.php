c2684989d94552331ef05379e52d8b8358d4925b
kaspar.keskula <kaspar.keskyla2@tptlive.ee>
2023-11-14 11:30:03 +0200

    made posts

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.4/css/bulma.min.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="content">
+            <?php if(have_posts()): ?>
+                <?php while(have_posts()): ?>
+                    <?php the_post(); ?>
+                    <div class="card mb-2">
+                        <header class="card-header">
+                            <p class="card-header-title">
+                                <?php the_title(); ?>
+                            </p>
+                        </header>
+                        <div class="card-content">
+                            <div class="content">
+                                <?php the_content(); ?>
+                            </div>
+                        </div>
+                    </div>
+                <?php endwhile; ?>
+            <?php endif; ?>
+        </div>
+    </div>
+</body>
+</html>

0da10f6a281c298007495f89e6178e34a2ee2f29
kaspar.keskula <kaspar.keskyla2@tptlive.ee>
2023-11-20 15:18:18 +0200

    asd

--- functions.php
+++ functions.php
@@ -14,5 +14,9 @@ function get_nav_items() {
     $locations = get_nav_menu_locations();
     $menu = wp_get_nav_menu_object($locations[$menu_slug_to_retrive]);
     return wp_get_nav_menu_items($menu->term_id);
-    
+
+}
+if(!is_admin()){
+    wp_enqueue_style('mystyle', get_template_directory_uri() . '/mystyle.css');
+    wp_enqueue_script('myscript', get_template_directory_uri() . '/script.js');
 }
